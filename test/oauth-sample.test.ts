3e09c15def75d490270005c45c7bdb9177af3521
Sieg Collado <siegcollado@gmail.com>
2022-02-16 17:08:50 +0800

    added test data for valid client id and secret combination

--- test/oauth-sample.test.ts
+++ test/oauth-sample.test.ts
@@ -1,25 +1,41 @@
+import 'jest-extended'
 import { oAuthRequest } from './helpers/test-helpers'
 import * as jwt from 'jsonwebtoken'
+
 describe('testing oauth credentials', () => {
-  describe('happy path', () => {
+  describe.each(
+    [
+      ['KRpatygJ3C5xvWxy4UuLkdm5qXMhCvc5', 'wo-gIcYoQjn-dmGgRe_-pYIZTvgWsA_3tDUOqFLwJpIJdD-wHeDuiQrxrXQor3_X', ['get:clients', 'get:invoices']],
+      ['663uIPmEUmVw2T8JVK3S9zoIgmTU4RT4', 'lsyjKKZDmAOKM39m2ry-4DsdF1jQMsdOh6fUInkyiy7NrL-e2tp1WIByFu72wWkm', ['get:clients']],
+    ]
+  )('given a client id is %p and client secret is %p ', (clientId, clientSecret, allowedScopes) => {
 
     let responseText: any
 
     beforeAll(async () => {
       responseText = await oAuthRequest({
         grant_type: 'client_credentials',
-        client_id: 'KRpatygJ3C5xvWxy4UuLkdm5qXMhCvc5',
-        client_secret: 'wo-gIcYoQjn-dmGgRe_-pYIZTvgWsA_3tDUOqFLwJpIJdD-wHeDuiQrxrXQor3_X',
+        client_id: clientId,
+        client_secret: clientSecret,
         audience: 'https://test-data-api.com'
       })
     })
 
-    it('should be a valid jwt', () => {
+    test('response contains the required properties', () => {
+      expect(responseText).toEqual(expect.objectContaining({
+        access_token: expect.any(String),
+        expires_in: expect.any(Number),
+        token_type: expect.any(String),
+        scope: expect.any(String)
+      }))
+    })
+
+    test('access_token should decode to a valid jwt', () => {
       expect(() => jwt.decode(responseText['access_token'])).not.toThrow()
     })
 
-    describe('checking for properties', () => {
-      let decodedValue: unknown
+    describe('checking JWT properties', () => {
+      let decodedValue: any
 
       beforeEach(() => {
         decodedValue = jwt.decode(responseText['access_token'])
@@ -51,6 +67,10 @@ describe('testing oauth credentials', () => {
         const computedExpiry = exp - iat
 
         expect(computedExpiry).toBe(expiresIn)
+      })
+
+      it('contains the correct scopes', () => {
+        expect(decodedValue['permissions']).toIncludeSameMembers(allowedScopes)
       })
     })
   })
